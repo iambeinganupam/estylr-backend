0e1bc03cd55d3a9632783dcbc5344591db38bd15
Anupam Raj <166903010+iambeinganupam@users.noreply.github.com>
2026-07-21 22:22:52 +0530

    Delete backend/040_audit_log.down.sql

--- backend/040_audit_log.down.sql
+++
@@ -1,6 +0,0 @@
--- Reverses 040_audit_log.up.sql
-
-DROP TRIGGER IF EXISTS trg_audit_log_no_delete ON public.audit_log;
-DROP TRIGGER IF EXISTS trg_audit_log_no_update ON public.audit_log;
-DROP FUNCTION IF EXISTS public.audit_log_block_mutation();
-DROP TABLE  IF EXISTS public.audit_log;
